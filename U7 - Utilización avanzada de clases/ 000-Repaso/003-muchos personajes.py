4373bf07d9acfd26b5e4b22b5d1f19995481d6a9
TavoDev <gustavoedv35@gmail.com>
2025-11-28 10:27:54 +0100

    1

---
+++ U7 - Utilización avanzada de clases/ 000-Repaso/003-muchos personajes.py
@@ -0,0 +1,16 @@
+#non playable character
+
+class Npc():
+	def __init__(self,x,y):
+		self.posx = x
+		self.posy = y
+		
+personajes = []
+numero_personajes = 50
+
+for i in range(0,numero_personajes):
+	personajes.append(Npc(4,3))
+
+print(personajes)	
+
+
